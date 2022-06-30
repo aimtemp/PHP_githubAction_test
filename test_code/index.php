f87c5be73ae5500cdccad3888f231399cb138a30
aim <yoshihiro.takasuka@aimnext.com>
2022-06-30 11:49:48 +0900

    firstCommit

---
+++ test_code/index.php
@@ -0,0 +1,14 @@
+<?php
+class testClass{
+
+    public function add($x,$y) {
+        return $x+$y;
+    }
+    public function sub($x,$y) {
+        return $x-$y;
+    }
+    public function add2($x,$y) {
+        return $x+$y*5;
+    }
+}
+?>
